48a04f25a7ec9cb0674db851d3765cd91ebc7617
insightn777 <39460495+insightn777@users.noreply.github.com>
2021-07-02 18:34:05 +0900

    Update solve.py

--- boj_5427/solve.py
+++ boj_5427/solve.py
@@ -1,15 +1,13 @@
-import itertools
 from collections import deque
 
 
 def solve(col_cnt: int, row_cnt: int, field) -> int:
     deq = deque()
-    sg_deq = deque()
-    sg: tuple
+    sg_stack = []
     success = 0
 
-    visit_field = [ [0] * col_cnt ] * row_cnt
-    
+    visit_field = [[0 for col in range(col_cnt)] for row in range(row_cnt)]
+
     for r in range(row_cnt):
         for c in range(col_cnt):
             spot = field[r][c]
@@ -17,34 +15,35 @@ def solve(col_cnt: int, row_cnt: int, field) -> int:
                 deq.append((True, r, c, 0))
             elif spot == '@':
                 sg = (False, r, c, 1)
+                deq.append(sg)
+                visit_field[r][c] = 1
+                sg_stack.append(0)
 
-    deq.append(sg)
-    visit_field[sg[0]][sg[1]] = 1
-    sg_deq.append(0)
+    del spot
 
-    while len(deq) > 0 and len(sg_deq):
+    while len(deq) > 0 and sg_stack and success == 0:
         flag, row, col, sec = deq.popleft()
         if flag:
             visit_spot = [
-                (row - 1, col), 
-                (row + 1, col), 
-                (row, col + 1), 
+                (row - 1, col),
+                (row + 1, col),
+                (row, col + 1),
                 (row, col - 1)
             ]
 
             for r, c in visit_spot:
-                if r >= row_cnt or c <= col_cnt or c < 0 or r < 0:
+                if r >= row_cnt or c >= col_cnt or c < 0 or r < 0:
                     continue
                 elif field[r][c] == '.' or field[r][c] == '@':
-                    field[r][c] == '*'
+                    field[r][c] = '*'
                     deq.append((True, r, c, 0))
-            
+
         else:
-            sg_deq.popleft()
+            sg_stack.pop()
             visit_spot = [
-                (row - 1, col, sec + 1), 
-                (row + 1, col, sec + 1), 
-                (row, col + 1, sec + 1), 
+                (row - 1, col, sec + 1),
+                (row + 1, col, sec + 1),
+                (row, col + 1, sec + 1),
                 (row, col - 1, sec + 1)
             ]
 
@@ -53,21 +52,17 @@ def solve(col_cnt: int, row_cnt: int, field) -> int:
                     success = s
                     deq.clear()
                     break
-                elif field[r][c] == '.' and visit_field[spot] == 0:
-                    visit_field[spot] = 1
-                    deq.appned((False, r, c, s))
-                    sg_deq.append(0)
+                elif field[r][c] == '.' and visit_field[r][c] == 0:
+                    visit_field[r][c] = 1
+                    deq.append((False, r, c, s))
+                    sg_stack.append(0)
 
     return 'IMPOSSIBLE' if success == 0 else success
 
 
 if __name__ == '__main__':
-    n = input()
-    case = int(n)
-    for i in range(case):
-        c, r = input().split()
-        col = int(c)
-        row = int(r)
-        field = [ list(input()) for i in range(row) ]
-        ans = solve(col, row, field)
+    cases = int(input())
+    for case in range(cases):
+        input_c, input_r = input().split()
+        ans = solve(int(input_c), int(input_r), [list(input()) for r in range(int(input_r))])
         print(ans)
